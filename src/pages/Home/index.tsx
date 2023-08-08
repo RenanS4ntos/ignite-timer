46328531a21002109efc7ff22e5834418fe95c63
renanS4ntos <renan-bs@hotmail.com>
2023-08-07 21:18:36 -0300

    Páginas e rotas - Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,22 @@ export function Home() {
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Desenvolver uma aplicação web" />
+            <option value="Desenvolver uma aplicação mobile" />
+            <option value="Desenvolver uma aplicação web e mobile" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
